c438d28b7f054f2f28d19db1e1e1e930f0bf5552
Stephanie7l <100887395+Stephanie7l@users.noreply.github.com>
2025-10-23 16:13:32 -0300

    Atualizações do site

--- meu-blog/vite.config.js
+++ meu-blog/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/leonciolopes-blog/' // igual ao nome do repo
-})
+  base: "/", // igual ao nome do repo
+});
